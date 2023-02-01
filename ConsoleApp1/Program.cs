48a99cd49822bf95b50b615889fc491a6cea3286
Sankalp <sankalp.kulkarni05@gmail.com>
2023-02-01 21:26:18 +0530

    [Sankalp]-Flip Coin

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace ConsoleApp1
+﻿using BasicCoreProgram;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program ");
+
+            //Flip Coin 
+            FlipCoin flipcoin = new FlipCoin();
+            flipcoin.PercentageFlips(10);
         }
     }
 }
